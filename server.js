19b55b1ff1b0a9c89bee4d568002045c25055443
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-07-10 20:39:13 +0100

    Updated

--- server.js
+++ server.js
@@ -1,73 +1,89 @@
+const isDevelopment = process.env.NODE_ENV === 'development';
+const apiUrl = isDevelopment ? process.env.REACT_APP_API_URL : 'http://mmcode.io';
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const morgan = require("morgan");
-const login = require("./Routes/login.js");
-const signUp = require("./Routes/signup.js");
-const apiUrl = process.env.REACT_APP_API_URL;
+const morgan = require('morgan');
+const login = require('./Routes/login.js');
+const signUp = require('./Routes/signup.js');
+const cookieParser = require('cookie-parser');
 
-
-app.set("view engine", "ejs");
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
-  credentials: true,
-}));
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use(session({
-  key: "userID",
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 60 * 60 * 24,
-    sameSite: 'none',
-    secure: true
-  },
-}));
-
+app.use(
+  session({
+    key: 'userID',
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+      sameSite: 'none',
+      secure: true,
+    },
+  })
+);
 
 // Route for testing server status
-
 login(app);
 signUp(app);
 
-// Check if the user session exists and redirect to /platform if logged in
-app.get('/login', (req, res) => {
-  if (req.session.user) {
-    console.log('Logged in user:', req.session.user.username);
-    return res.redirect('/platform');
-  }
+app.post('/login', (req, res) => {
+  // Authenticate user and set session data
 
-  // Render the login page
+  // Set a cookie to remember the login
+  res.cookie('rememberLogin', true, {
+    maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie expiration time (30 days)
+    httpOnly: false, // Allowing access from JavaScript
+    secure: false, // Not enforcing HTTPS
+    sameSite: 'None', // Allowing cross-site cookies
+  });
+  console.log('Login cookie set:', req.cookies.rememberLogin);
+
+  // Redirect or send response
   // ...
 });
 
-// Check if the user session exists and redirect to /platform if logged in
 app.get('/signup', (req, res) => {
   if (req.session.user) {
     console.log('Logged in user:', req.session.user.username);
     return res.redirect('/platform');
   }
 
-  // Render the signup page
-  // ...
-});
+  // Process the signup form and create a new user
 
+  // Set the session data for the newly signed up user
+  const newUser = {
+    username: 'newUser',
+    fullName: 'New User',
+    // Include any other relevant user data
+  };
+  req.session.user = newUser;
+
+  // Redirect to the platform page or any other desired route
+  res.redirect('/platform');
+});
 app.get('/platform', (req, res) => {
-  if (req.session.user) {
+  if (req.session.user || hasRememberLoginCookie()) {
     console.log('Logged in user:', req.session.user.username);
-    // Render the platform page
-    // ...
+   
+    res.render('platform'); 
   } else {
-    // User is not logged in, redirect to the login page
     res.redirect('/login');
   }
 });
