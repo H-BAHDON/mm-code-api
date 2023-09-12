902084600baff8b478405931df91368cf5e23c2c
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-12 15:40:08 +0100

    Updated authController.js

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -9,17 +9,24 @@ function platform(req, res) {
 
 function getUser(req, res) {
   if (req.isAuthenticated()) {
-    // Assuming you have a function to retrieve user data from the database
     const userId = req.user.id; // Assuming you have a unique identifier for users
     db.query('SELECT full_name, email FROM users WHERE id = $1', [userId])
       .then(result => {
         if (result.rows.length === 0) {
-          res.status(404).json({ error: 'User not found in the database' });
+          // User not found in the database, use backup method
+          const userData = {
+            displayName: req.user.displayName || req.user.username, // Use username if displayName is not available
+            email: req.user.email,
+          };
+          req.session.userData = userData;
+          res.json(userData);
         } else {
+          // User found in the database
           const userData = {
             displayName: result.rows[0].full_name,
             email: result.rows[0].email,
           };
+          req.session.userData = userData;
           res.json(userData);
         }
       })
@@ -31,6 +38,7 @@ function getUser(req, res) {
     res.status(401).json({ error: 'Not authenticated' });
   }
 }
+
 
 
 function checkSession(req, res) {
