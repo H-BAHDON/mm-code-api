af069f2a58caadc7de9e59d6510f0c8029c0c255
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-12 15:33:48 +0100

    updated authController.js

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,4 +1,4 @@
-const db = require('../../config/db');
+const db = require('../../config/db/db');
 function platform(req, res) {
   req.session.randomValue = Math.random();
   const storedRandomValue = req.session.randomValue;
