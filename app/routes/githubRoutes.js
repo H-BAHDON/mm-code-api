8b253fc1ee388209fcc7754dc23ec45c88387aec
Hussein Bahdon <husseinbahdon1@gmail.com>
2023-09-14 13:01:44 +0100

    Revert "Creating end points"

--- app/routes/githubRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const githubController = require('../controllers/githubController');
-
-router.get('/auth/github', githubController.githubAuth);
-router.get('/auth/github/callback', githubController.githubCallback);
-router.get('/github/user', githubController.getGitHubUser);
-
-module.exports = router;
